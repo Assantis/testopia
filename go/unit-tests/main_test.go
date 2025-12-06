6517d7b5f1faee87b1c404020e684501e0a26e6c
Assantis <alexander@knakrick.de>
2025-12-06 17:18:18 +0000

    feat: add the first test

--- go/unit-tests/main_test.go
+++ go/unit-tests/main_test.go
@@ -1,1 +1,25 @@
 package main_test
+
+import (
+	"testing"
+	main "testopia/m/v2"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Let's start simple with a basic unit test
+// I like to explain tests with three simple terms:
+// Given / When / Then
+func TestGenerator(t *testing.T) {
+	// Given: your given environment or parameters for the test
+	generator := main.NewDefaultGenerator()
+
+	// When: the exuction of a function or method
+	result := generator.GenerateItem()
+
+	// Then: the assertions you want to make about the result
+	require.NotEmpty(t, result.Name)
+	require.NotEmpty(t, result.Currency)
+	require.NotZero(t, result.Value)
+	require.NotEmpty(t, result.Tags)
+}
